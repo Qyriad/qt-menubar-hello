7c387dc7afd20d690393e0939e0c8912dad3ae74
Qyriad <qyriad@qyriad.me>
2023-09-25 14:29:28 -0600

    add -Dqt_version to switch between Qt5 and Qt6

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
         pkgs = import nixpkgs { inherit system; };
         inherit (builtins) path attrValues;
 
-        menubar = pkgs.stdenv.mkDerivation {
+        menubar-qt5 = pkgs.stdenv.mkDerivation {
           name = "menubar";
 
           src = path {
@@ -31,7 +31,7 @@
           };
 
           buildInputs = attrValues {
-            inherit (pkgs.libsForQt5)
+            inherit (pkgs.qt5)
               qtbase
               qtwayland
               qttools
@@ -45,11 +45,66 @@
           '';
 
         };
-      in {
-        packages.default = menubar;
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ menubar ];
+
+        menubar-qt6 = pkgs.stdenv.mkDerivation {
+          name = "menubar";
+
+          src = path {
+            name = "menubar-src";
+            path = ./.;
+          };
+
+          nativeBuildInputs = attrValues {
+            inherit (pkgs)
+              meson
+              ninja
+              pkg-config
+            ;
+            inherit (pkgs.qt6)
+              qmake
+              wrapQtAppsHook
+            ;
+          };
+
+          buildInputs = attrValues {
+            inherit (pkgs.qt6)
+              qtbase
+              qtwayland
+              qttools
+            ;
+          };
+
+          mesonFlags = [ "-Dqt_version=qt6" ];
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp ./compile_commands.json $out/
+            cp ./menubar $out/bin/
+          '';
+
         };
+      in {
+
+        packages = {
+          default = menubar-qt5;
+          inherit menubar-qt5 menubar-qt6;
+        };
+
+        devShells = {
+          default = pkgs.mkShell {
+            inputsFrom = [ menubar-qt5 ];
+          };
+
+          menubar-qt5 = pkgs.mkShell {
+            inputsFrom = [ menubar-qt5 ];
+          };
+
+          menubar-qt6 = pkgs.mkShell {
+            inputsFrom = [ menubar-qt6 ];
+          };
+
+        };
+
       }
     ) # eachDefaultSystem
   ; # outputs
